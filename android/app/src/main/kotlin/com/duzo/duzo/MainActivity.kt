92baa25ab900685c16a0a9fd1de5320aa7e503f4
madhu <dmpathani@gmail.com>
2024-11-01 21:54:04 +0530

    removed many duplicate lines from conflict

--- android/app/src/main/kotlin/com/duzo/duzo/MainActivity.kt
+++ android/app/src/main/kotlin/com/duzo/duzo/MainActivity.kt
@@ -3,9 +3,3 @@ package com.duzo.duzo
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
-
-package com.duzo.duzo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
